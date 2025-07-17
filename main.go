1d7508f7be40a773e37431c896a23af697e165c9
perdidev <perdidev@gmail.com>
2025-07-17 21:00:52 +0800

    setup server

--- main.go
+++ main.go
@@ -1,4 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"time"
+	"net/http"
+	"github.com/HySkySFC/hyskysfc-backend-api/internal/app"
+)
+
 func main() {
+	app, err := app.NewApplication()
+	if err != nil {
+		panic(err)
+	}
+
+	app.Logger.Println("we are running our app")
+
+	http.HandleFunc("/health", HealthCheck)
+	server := &http.Server{
+		Addr: ":8080",
+		IdleTimeout: time.Minute,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "status is available")
 }
